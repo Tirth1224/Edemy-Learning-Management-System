620d7f98907aa4379a774b5c6a8cf1aa9185966a
Tirth Patel <tirthp292003@gmail.com>
2025-04-10 15:05:47 +0530

    Add Backend

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -6,6 +6,6 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () =>
     console.log("Database connected successfully!")
   );
-  await mongoose.connect(`${process.env.MONGODB_URI}/Edemy-LMS`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/Edemy`);
 };
 export default connectDB;
