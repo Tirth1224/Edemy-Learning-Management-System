801a20befbf20cb7c79687800b5d3afefa4375c3
Tirth Patel <tirthp292003@gmail.com>
2025-05-02 15:40:49 +0530

    Remove Comments and Unused code

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,33 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.JPG"],
   server: {
     hmr: {
-      overlay: false, // Disable error overlay
+      overlay: false,
     },
   },
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import path from "path";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   assetsInclude: ["**/*.JPG"],
-//   server: {
-//     hmr: {
-//       overlay: false, // Disable error overlay
-//     },
-//   },
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "src"),
-//     },
-//   },
-// });
